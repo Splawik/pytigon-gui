6274c920f8af63e34de0d49e0670acc1f53d4bb8
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2019-09-22 17:35:11 +0200

    Added: setup.py, LICENSE

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytigon-gui",
+    version="0.9",
+    description="Pytigon GUI",
+    author="Sławomir Chołaj",
+    author_email="slawomir.cholaj@gmail.com",
+    license="LGPLv3",
+    packages=find_packages(),
+    install_requires=["pytigon", "wxpython", "wxasync", "PyDispatcher", "cefpython3"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
