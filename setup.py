588d347c532d8d5df6b49720c03332ad0bb67fdf
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2020-02-16 19:13:11 +0100

    v0.96

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytigon-gui",
-    version="0.95",
+    version="0.96",
     description="Pytigon GUI",
     author="Sławomir Chołaj",
     author_email="slawomir.cholaj@gmail.com",
